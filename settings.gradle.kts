fedda5245b108e5f108061214df22105e33d5c90
Sergei Miroshkin <miroshkinsergei.3@gmail.com>
2022-04-14 13:12:14 +0300

    feature: finished migrate to kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+rootProject.name = "ShogunNotesApp"
+
+include (":presentation")
+include (":data")
+include (":domain")
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -12,7 +18,3 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "ShogunNotesApp"
-include ':presentation'
-include ':data'
-include ':domain'
